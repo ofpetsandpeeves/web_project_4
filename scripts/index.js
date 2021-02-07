6d0889cdf745f43f8481846949a550c8e511727f
OfPetsandPeeves <tpizzo@tuta.io>
2021-02-06 21:43:24 -0600

    Added modal functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,28 @@
-const openPopup = document.querySelector(".edit-button");
+// Modal related elements
+let openPopup = document.querySelector(".edit-button");
+let popupActive = document.querySelector(".popup");
+let closePopup = document.querySelector(".close-btn");
+let saveButton = document.querySelector(".submit-button");
 
-openPopup.addEventListener("click", functuon () {
+// Events handling open and closing of modal
+openPopup.addEventListener("click", function() {
+    popupActive.classList.add("popup_opened");
+});
 
-} )
+closePopup.addEventListener("click", function () {
+    popupActive.classList.remove("popup_opened");
+});
+
+saveButton.addEventListener("click", function () {
+    popupActive.classList.remove("popup_opened");
+});
+
+// Form related elements 
+let form = document.querySelector(".popup__form");
+
+// Events handling form input
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+});
+
+
