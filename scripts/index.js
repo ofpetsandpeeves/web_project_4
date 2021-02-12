8eafe411d5a8c0c27b76e6f6760486b7f63ed49a
OfPetsandPeeves <tpizzo@tuta.io>
2021-02-11 19:43:12 -0600

    hotfix: update JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,21 @@
 let form = document.querySelector(".popup__form");
 let formNameInput = document.querySelector(".popup__form-control_type_name");
 let formAboutMeInput = document.querySelector(".popup__form-control_type_about");
-
 let profileName = document.querySelector(".profile__title");
 let profileAboutMe = document.querySelector(".profile__subtitle");
-
 let popup = document.querySelector(".popup"); //use when direct changes to modal
-
 let onEdit = document.querySelector(".edit-button"); // when you click edit this happens
 let onClose = document.querySelector(".close-btn");
+
+function textSavedContent() {
+  profileName.textContent = formNameInput.value; //equal to the value
+  profileAboutMe.textContent = formAboutMeInput.value;
+}
+
+function textNotSaved() {
+  formNameInput.value = profileName.textContent;
+  formAboutMeInput.value = profileAboutMe.textContent;
+}
 
 
 function ravenOpenModal(e) {
@@ -16,20 +23,17 @@ function ravenOpenModal(e) {
 
   if (!open) {
     e.preventDefault();
-    profileName.textContent = formNameInput.value; //equal to the value
-    profileAboutMe.textContent = formAboutMeInput.value;
+    textSavedContent();
 
   } else {
-    formNameInput.value = profileName.textContent;
-    formAboutMeInput.value = profileAboutMe.textContent;
-  }};
+    textNotSaved();
+}};
 
-function exitWithoutSave () {
+function exitWithoutSave() {
   let open = popup.classList.toggle("popup_opened");
 
   if (!open) {
-    formNameInput.value = profileName.textContent;
-    formAboutMeInput.value = profileAboutMe.textContent;
+    textNotSaved();
 }};
 
 
@@ -37,6 +41,7 @@ function exitWithoutSave () {
 onEdit.addEventListener("click", ravenOpenModal);
 onClose.addEventListener("click", exitWithoutSave);
 form.addEventListener("submit", ravenOpenModal);
+
 
 
 
